955edea1b1810a16e837e43e5b2749b1d51bb736
Oliver J. Woodford <o.j.woodford.98@cantab.net>
2022-07-18 22:42:31 +0100

    Add src and test code

--- src/UserConfig.jl
+++ src/UserConfig.jl
@@ -1,5 +1,111 @@
 module UserConfig
+export localstore, string2key, localpath, localstring 
 
-# Write your package code here.
+import NativeFileDialog, JLD2 
+
+"""
+    localstore(strname, data)      # Store data
+    data = localstore(strname)     # Read data
+    localstore(strname, "delete")  # Delete data
+
+Store and get user config data defined by `strname`.
+"""
+function localstore(strname::String, data::Any="")
+    fname = joinpath((DEPOT_PATH[1], "config", string(strname, ".jld2")))
+    if data === ""
+        # Reading the data
+        if isfile(fname)
+            try
+                return JLD2.read(JLD2.jldopen(fname, "r"), "data")
+            catch err
+                printstyled(err)
+            end
+        end
+    elseif data === "delete"
+        rm(fname, force=true)
+    else
+        # Writing the data
+        try
+            dname = dirname(fname)
+            if !isdir(dname)
+                mkdir(dname)
+            end
+            JLD2.jldsave(fname; data)
+            return data
+        catch err
+            printstyled(err)
+        end
+    end
+    # Failure case
+    return ""
+end
+
+"""
+    key = string2key(string)
+
+Convert a string to something suitable as a human-readable key, by removing spaces and special characters.
+"""
+function string2key(title::String)
+    key = string(strip(replace(lowercase(title), " "=>"-", r"[^(a-z0-9)]"=>""), '-'))
+    if '0' <= key[1] <= '9'
+        key = string('n', key)
+    end
+    return key
+end
+
+"""
+    path = localpath(title, checkfun, isfolder=false)
+
+Get the path to a local file or folder (if isfolder is true), and ask the user if they haven't given it before.
+checkfun(path) is run, and only returns the path if true.
+"""
+function localpath(title::String, checkfun::Function, isfolder::Bool=false)
+    fname = string2key(title)
+    while true
+        # Get the stored string
+        strout = localstore(fname)
+        if checkfun(strout)
+            return strout
+        end
+        # Ask the user for the path
+        if isfolder
+            strout = NativeFileDialog.pick_folder(string("Select the ", title, " folder"))
+            if !isdir(strout)
+                throw(DomainError("directory not selected"))
+            end
+        else
+            strout = NativeFileDialog.pick_file(string("Select the ", title, " file"))
+            if !isfile(strout)
+                throw(DomainError("file not selected"))
+            end
+        end
+        if isempty(localstore(fname, strout))
+            throw(DomainError("unable to store string"))
+        end
+    end
+end
+
+"""
+    string = localstring(title, checkfun=s->true)
+
+Get a user specific string, and ask the user if they haven't given it before.
+checkfun(string) is run, and only returns the string if true.
+"""
+function localstring(title::String, checkfun::Function=s->true)
+    fname = string2key(title)
+    while true
+        # Get the stored string
+        strout = localstore(fname)
+        if !isempty(strout) && checkfun(strout)
+            return strout
+        end
+        # Ask the user for the string
+        println("Please enter your ", title, ":")
+        strout = readline()
+        if isempty(localstore(fname, strout))
+            throw(DomainError("unable to store string"))
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,21 @@ using UserConfig
 using Test
 
 @testset "UserConfig.jl" begin
-    # Write your tests here.
+    # Test the filename conversions
+    @test string2key("! User Config Test ^") === "user-config-test"
+    @test string2key("123 User Config Test&*!") === "n123-user-config-test"
+
+    # Test the saving and loading of data
+    teststring = "C:\\mypath/myfile.ext"
+    testdict = Dict("array"=>randn(3, 4), "string"=>teststring)
+    @test localstore("user-config-test", "delete") === ""
+    @test localstore("user-config-test") === ""
+    @test localstore("user-config-test", teststring) === teststring
+    @test localstore("user-config-test") === teststring
+    @test localstore("user-config-test", testdict) === testdict
+    @test localstore("user-config-test") == testdict
+    @test localstore("user-config-test", "delete") === ""
+    @test localstore("user-config-test") === ""
+
+    # Don't test the interactive methods
 end
